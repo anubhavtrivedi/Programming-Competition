850cf42e28c008d20f29537772f50dfba514df4c
Anubhav Trivedi <anu.bhavtrivedi111@gmail.com>
2017-12-29 01:24:11 +0530

    Update Three Garlands.java

--- Three Garlands.java
+++ Three Garlands.java
@@ -1,5 +1,28 @@
 import java.io.*;
 import java.util.*;
+/*
+Mishka is decorating the Christmas tree. He has got three garlands, and all of them will be put on the tree. After that Mishka will
+switch these garlands on.
+
+When a garland is switched on, it periodically changes its state — sometimes it is lit, sometimes not. Formally, if i-th garland is 
+switched on during x-th second, then it is lit only during seconds x, x + ki, x + 2ki, x + 3ki and so on.
+
+Mishka wants to switch on the garlands in such a way that during each second after switching the garlands on there would be at least 
+one lit garland. Formally, Mishka wants to choose three integers x1, x2 and x3 (not necessarily distinct) so that he will switch on the
+first garland during x1-th second, the second one — during x2-th second, and the third one — during x3-th second, respectively, and 
+during each second starting from max(x1, x2, x3) at least one garland will be lit.
+
+Help Mishka by telling him if it is possible to do this!
+
+Input
+The first line contains three integers k1, k2 and k3 (1 ≤ ki ≤ 1500) — time intervals of the garlands.
+
+Output
+If Mishka can choose moments of time to switch on the garlands in such a way that each second after switching the garlands on at least
+one garland will be lit, print YES.
+
+Otherwise, print NO.
+*/
 public class GFG {
 	public static void main (String[] args) {
 		Scanner in = new Scanner(System.in);
