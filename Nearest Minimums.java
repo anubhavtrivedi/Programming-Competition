859cbebbdd9b3cbec830fa3dd3391a438c10b9de
Anubhav Trivedi <anu.bhavtrivedi111@gmail.com>
2017-12-29 01:22:37 +0530

    Update Nearest Minimums.java

--- Nearest Minimums.java
+++ Nearest Minimums.java
@@ -1,5 +1,17 @@
 import java.io.*;
 import java.util.*;
+/*
+You are given an array of n integer numbers a0, a1, ..., an - 1. Find the distance between two closest (nearest) minimums in it.
+It is guaranteed that in the array a minimum occurs at least two times.
+
+Input
+The first line contains positive integer n (2 ≤ n ≤ 105) — size of the given array. The second line contains
+n integers a0, a1, ..., an - 1 (1 ≤ ai ≤ 109) — elements of the array. It is guaranteed that in the array a minimum occurs at least two
+times.
+
+Output
+Print the only number — distance between two nearest minimums in the array.
+*/
 public class GFG {
 	public static void main (String[] args) {
 		Scanner in = new Scanner(System.in);
