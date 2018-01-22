b279309ca3ae138c6882051694895315b8359592
Anubhav Trivedi <anu.bhavtrivedi111@gmail.com>
2018-01-23 04:16:41 +0530

    Update Conan and Agasa play a Card Game.java

--- Conan and Agasa play a Card Game.java
+++ Conan and Agasa play a Card Game.java
@@ -1,3 +1,14 @@
+/*
+Edogawa Conan got tired of solving cases, and invited his friend, Professor Agasa, over. They decided to play a game of cards. Conan has
+n cards, and the i-th card has a number ai written on it.
+
+They take turns playing, starting with Conan. In each turn, the player chooses a card and removes it. Also, he removes all cards having a 
+number strictly lesser than the number on the chosen card. Formally, if the player chooses the i-th card, he removes that card and removes
+the j-th card for all j such that aj < ai.
+
+A player loses if he cannot make a move on his turn, that is, he loses if there are no cards left. Predict the outcome of the game, 
+assuming both players play optimally.
+*/
 import java.io.*;
 import java.util.*;
 public class GFG {
