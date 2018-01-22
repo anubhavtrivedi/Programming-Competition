0297be9b612aa5fce1095bffd211f0bdfc7b0751
Anubhav Trivedi <anu.bhavtrivedi111@gmail.com>
2018-01-23 04:20:31 +0530

    Update Modular Exponentiation.java

--- Modular Exponentiation.java
+++ Modular Exponentiation.java
@@ -1,27 +1,29 @@
+
+/*
+The following problem is well-known: given integers n and m, calculate
+
+,2^n mod m
+where 2n = 2·2·...·2 (n factors), and  denotes the remainder of division of x by y.
+
+You are asked to solve the "reverse" problem. Given integers n and m, calculate
+m mod 2^n
+
+.
+*/
 import java.io.*;
 import java.util.*;
 public class GFG {
 	public static void main (String[] args) {
 		Scanner in = new Scanner(System.in);
-	int a;
-	a=in.nextInt();
-	if(a==1){
-	System.out.println("Conan");
-	return;}
-	int l;
-	int[] b=new int[100009];
-	for(int i=0;i<a;i++){
-	    
-	    b[in.nextInt()]++;
-	}
+		int n,m;
+		long n0;
+		n=in.nextInt();
+		m=in.nextInt();
+		n0=(long)Math.pow(2,n);
+		System.out.println(m%n0);
+	   
+}
+}
 
 
- for(int x:b){
-     if(x%2!=0){
-         System.out.println("Conan");
-         return;
-     }
- }
-
-System.out.println("Agasa");
 
